de974fb061a0c1df376a2f2894eb15b0fd577729
JohnHarlepas <harlepas@gmail.com>
2019-02-06 17:09:19 -0500

    added weather API

--- public/assets/javascript/website.js
+++ public/assets/javascript/website.js
@@ -151,4 +151,134 @@ $(document).ready(function () {
 
 
   // John H.
+  // weather widget 
+  // this is the api provided by darksky 
+
+  var APIKey1 = "c1bb514e192e30552256903a2f6dbb49";
+
+  // Here we are building the URL we need to query the database. You need to use the proxy to avoid the CORS error. Go here for more info:https://www.freecodecamp.org/forum/t/solved-having-trouble-getting-response-from-dark-sky-api/100653/2
+  var proxy =  "https://cors-anywhere.herokuapp.com/";
+  var queryURL = "https://api.darksky.net/forecast/" + APIKey1 + "/38.846218,-77.306374"
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: proxy + queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".probRain").html(response.currently.precipProbability + "%");
+      $(".cityTemp").html(response.currently.temperature + " °F");
+
+      // Log the data in the console as well
+      console.log("Prob. Rain: " + response.currently.precipProbability);
+      // console.log("Humidity: " + response.main.humidity);
+      // console.log("Temp (F): " + response.main.temp);
+      // console.log("Clouds: " + response.clouds + "%")
+    });
+
+
+// --------------------------------------------------------------------------------------------------------
+// This code works but for OpenWeather. We already used the API so I needed to chose another one.
+  // var APIKey = "a23739c0f73bab1e826b057e9e677284";
+
+  // Here we are building the URL we need to query the database
+  // var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  //   "q=Fairfax,US&units=imperial&appid=" + APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // })
+    // We store all of the retrieved data inside of an object called "response"
+    // .then(function(response) {
+
+      // Log the queryURL
+      // console.log(queryURL);
+
+      // Log the resulting object
+      // console.log(response);
+
+      // Transfer content to HTML
+      // $(".cityInput").html(response.name + ", Virginia");
+      // $(".wind").text("Wind Speed: " + response.wind.speed);
+      // $(".cityClouds").text(response.clouds.all + "%");
+      // $(".cityTemp").text(response.main.temp + " °F");
+
+      // Log the data in the console as well
+    //   console.log("Wind Speed: " + response.wind.speed);
+    //   console.log("Humidity: " + response.main.humidity);
+    //   console.log("Temp (F): " + response.main.temp);
+    //   console.log("Coverage: " + response.clouds + "%")
+    // });
+
+    // display time and date in nav bar
+    function displayRealTime() {
+      setInterval(function(){
+          $('.timeInput').html(moment().format('hh:mm A'))
+        }, 1000);
+      }
+      displayRealTime();
+      
+
+    function displayRealDate() {
+        
+            $('.dateInput').html(moment().format('MMM Do YYYY'));
+        
+        }
+        displayRealDate();
+
+
+
+  // this is the 3d flip Box  for yelp review
+
+
+
+
+
+ // yelp reviews 
+  // this is the api provided by darksky 
+
+  var APIKey2 = "ZAjQEsHcP3gXuoxr-iw9hHU1_5LX-Nc5Qqg2_M2Hv41cBy8DT7QVq7nu64-I-NVIWThqYJwEb08BDgD2HVQv-sdr4TaZvVsCNWoY51mO3YTarER2ps9da2cck0tbXHYx";
+
+  // Here we are building the URL we need to query the database. You need to use the proxy to avoid the CORS error. Go here for more info:https://www.freecodecamp.org/forum/t/solved-having-trouble-getting-response-from-dark-sky-api/100653/2
+  // var proxy =  "https://cors-anywhere.herokuapp.com/";
+  //www.yelp.com/biz/ace-mobile-auto-detailing-fairfax
+  var proxy =  "https://cors-anywhere.herokuapp.com/";
+  var queryURL = "https://api.yelp.com/v3/businesses/f5CEHI2aNAriY23TObBQxw/reviews/" + APIKey1
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: proxy + queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      // $(".probRain").html(response.currently.precipProbability + "%");
+      // $(".cityTemp").html(response.currently.temperature + " °F");
+
+      // Log the data in the console as well
+      // console.log("Prob. Rain: " + response.currently.precipProbability);
+      // console.log("Humidity: " + response.main.humidity);
+      // console.log("Temp (F): " + response.main.temp);
+      // console.log("Clouds: " + response.clouds + "%")
+    });
+
 
